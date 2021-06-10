ea9ccf3fd447b4222582b25394227039eea33296
Rodrigo Affonso <profrodrigoaffonso@gmail.com>
2021-06-10 14:53:08 -0300

    First commit

---
+++ app/Http/Controllers/Commands.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Commands extends Controller
+{
+    //
+    public function send(){
+        echo 'teste';
+        die;
+    }
+}
---
+++ database/migrations/2021_06_09_220317_create_commands_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commands', function (Blueprint $table) {
+            $table->id();
+            $table->string('command', 20)->nullable();
+            $table->enum('executed', ['y','n'])->default('n');
+            $table->timestamps();
+        });
+
+        DB::table('commands')->insert(
+            [
+                'command' => null
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commands');
+    }
+}
