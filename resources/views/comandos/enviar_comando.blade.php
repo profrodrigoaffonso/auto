7949fffeee870482a481559c11f3d7667540a7cc
Rodrigo Affonso <profrodrigoaffonso@gmail.com>
2021-06-12 16:58:16 -0300

    Atualizacao

--- resources/views/comandos/enviar_comando.blade.php
+++ resources/views/comandos/enviar_comando.blade.php
@@ -3,12 +3,8 @@
 @section('content')
 <form action="{{ route('comandos.executar') }}" id="form" method="POST">
     @csrf
-    <div class="form-group">
       <input type="hidden" class="form-control" id="comando" name="comando">
-      @if($errors->has('codigo'))
-      Digite o código
-      @endif
-    </div>
+
     <button type="button" class="btn btn-lg btn-success btn-block" onclick="send('on')">ON</button><br><br>
     <button type="button" class="btn btn-lg btn-danger btn-block" onclick="send('off')">OFF</button>
 </form>
